2bfd0285624db74e40f9b364246cf5f2f1e73a66
DanielPBak <DanielPBak@gmail.com>
2016-07-01 20:02:39 -0300

    Added retreat AI
    Soldiers' highest priority is to move to any flag named "RetreatFlag".

--- role.soldier.grunt.js
+++ role.soldier.grunt.js
@@ -2,11 +2,17 @@ module.exports = {
     run: function (creep) {
         var militaryFlag = Game.flags["MilitaryFlag"];
         var killFlag = Game.flags["KillFlag"];
+        var retreatFlag = Game.flags["RetreatFlag"];
         var closestEnemyCreep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         var closestEnemySpawn = creep.pos.findClosestByRange(FIND_HOSTILE_SPAWNS);
+        // var closestEnemyTower = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {filter: str => str.structureType == STRUCTURE_TOWER});
 
 
-        if (killFlag != null) {
+        if (retreatFlag != null) {
+            creep.moveTo(retreatFlag);
+        }
+
+        else if (killFlag != null) {
             var toKill = killFlag.pos.findInRange(FIND_STRUCTURES, 0)[0];
 
             if (creep.attack(toKill) == ERR_NOT_IN_RANGE) {

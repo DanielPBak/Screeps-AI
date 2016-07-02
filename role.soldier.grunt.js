267a49f9165983a0d092a568ea5b3d93e9e90594
DanielPBak <DanielPBak@gmail.com>
2016-07-02 18:54:30 -0300

    Added in combat flag, changed toKill flag to work across rooms.

--- role.soldier.grunt.js
+++ role.soldier.grunt.js
@@ -3,20 +3,31 @@ module.exports = {
         var militaryFlag = Game.flags["MilitaryFlag"];
         var killFlag = Game.flags["KillFlag"];
         var retreatFlag = Game.flags["RetreatFlag"];
+        var gruntInCombatFlag = Game.flags["GruntCombatFlag"];
         var closestEnemyCreep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         var closestEnemySpawn = creep.pos.findClosestByRange(FIND_HOSTILE_SPAWNS);
         // var closestEnemyTower = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {filter: str => str.structureType == STRUCTURE_TOWER});
 
 
-        if (retreatFlag != null) {
+        if (gruntInCombatFlag != null){
+            creep.moveTo(gruntInCombatFlag);
+        }
+
+        else if (retreatFlag != null) {
             creep.moveTo(retreatFlag);
         }
 
         else if (killFlag != null) {
-            var toKill = killFlag.pos.findInRange(FIND_STRUCTURES, 0)[0];
 
-            if (creep.attack(toKill) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(toKill);
+            try {
+                var toKill = killFlag.pos.findInRange(FIND_STRUCTURES, 0)[0];
+
+                if (creep.attack(toKill) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(toKill);
+                }
+            }
+            catch(err){
+                creep.moveTo(killFlag);
             }
         }
 
